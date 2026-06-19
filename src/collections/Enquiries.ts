771a859b7703f2eec3f33894097b25421fe83185
anas <anassayyed000@gmail.com>
2026-06-19 22:55:50 +0530

    feat: initialize project structure with Payload CMS integration and project detail page components

--- src/collections/Enquiries.ts
+++ src/collections/Enquiries.ts
@@ -1,48 +1,58 @@
 import type { CollectionConfig, CollectionSlug } from "payload";
 
 export const Enquiries: CollectionConfig = {
-    slug: 'enquiries',
-    admin: {
-        group : 'leads',
-        defaultColumns: ['name', 'email', 'phone', 'status' , 'createdAt'],
-        useAsTitle: 'name',
+  slug: "enquiries",
+  admin: {
+    group: "leads",
+    defaultColumns: ["name", "email", "phone", "status", "createdAt"],
+    useAsTitle: "name",
+  },
+  access: {
+    read: ({ req }) => !!req.user,
+    create: () => true,
+    update: ({ req }) => !!req.user,
+    delete: ({ req }) => !!req.user,
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
     },
-    access: {
-        read : ({req}) => !!req.user,
-        create: () => true,
-        update: ({req}) => !!req.user,
-        delete: ({req}) => !!req.user,
+    {
+      name: "email",
+      type: "text",
+      required: true,
     },
-    fields: [
-        {
-            name: 'name', type: 'text', required: true
-        },
-        {
-            name: 'email',type: 'text', required: true
-        },
-        {
-            name: 'phone',type: 'text', required: true
-        },
-        {
-            name: 'projectInterestedIn',
-            type: 'relationship',
-            relationTo: 'projects' as unknown as CollectionSlug,
-        },
-        {
-            name: 'message', type: 'textarea', required: true
-        },
-        {
-            name: 'status',
-            type: 'select',
-            defaultValue: 'new',
-            options: ['new' , 'contacted' , 'closed'],
-
-        },
-        {
-            name: 'source', type: 'text' ,defaultValue: 'website'
-        },
-        {
-            name: 'notes' , type : 'textarea'
-        },
-    ],
-}
+    {
+      name: "phone",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "projectInterestedIn",
+      type: "relationship",
+      relationTo: "projects" as unknown as CollectionSlug,
+    },
+    {
+      name: "message",
+      type: "textarea",
+      required: true,
+    },
+    {
+      name: "status",
+      type: "select",
+      defaultValue: "new",
+      options: ["new", "contacted", "closed"],
+    },
+    {
+      name: "source",
+      type: "text",
+      defaultValue: "website",
+    },
+    {
+      name: "notes",
+      type: "textarea",
+    },
+  ],
+};
